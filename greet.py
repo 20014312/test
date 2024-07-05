5f7beb3644af9ccf56a77ef419bdf03cd8e04e16
20014312 <168001795+20014312@users.noreply.github.com>
2024-07-05 10:31:53 +0100

    Update greet.py

--- greet.py
+++ greet.py
@@ -1,4 +1,4 @@
-from flask import Flask
+rom flask import Flask
 from flask import request
 app = Flask(__name__)
 @app.route("/")#URL leading to method
@@ -9,4 +9,4 @@ def helloall(): # different method name
  name = request.args.get('name')#retrieve GET parameters
  return("Hello {}!".format(name))#Python’s string.format
 if __name__ == "__main__":
- app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem'))
+ app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
